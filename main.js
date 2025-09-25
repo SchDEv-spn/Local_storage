65a9cb5c9f8cf94a3762f22f867ce24106f31680
schneider manrique <schneider.manrique@gmai.com>
2025-09-25 14:02:04 -0500

    realice guardar curso en LocalStorage y mostrar detalles en pantalla

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-const form = document.getElementById('formulario')
-const input = document.getElementById('input')
-const saludo = document.getElementById('saludo')
-const borrar = document.getElementById('borrar')
+const form = document.getElementById('formulario-curso')
+const inpcurso = document.getElementById('curso')
+const inpprofesor = document.getElementById('profesor')
+const inpprecio = document.getElementById('precio')
+const inpciudad = document.getElementById('ciudad')
+const inpcupo = document.getElementById('cupo')
+const mensaje = document.getElementById('mensaje-curso')
+const btnBorrar = document.getElementById('borrar-curso')
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault
-    const nombre = input.value.trim();
 
-    if (nombre == "") {
-        alert('Ingrese su nombre pendejo')
-        return;
+    const curso = inpcurso.value.trim();
+    const profesor = inpprofesor.value.trim();
+    const precio = inpprecio.value.trim();
+    const ciudad = inpciudad.value.trim();
+    const cupo = inpcupo.value.trim();
+    if (curso == "" || profesor == ""|| precio == "" || ciudad == "" || cupo == "") {
+        alert('POr favor rellenar todos los campos')
+        return
     }
-    // difinimos clase y valor
-    localStorage.setItem('usuario', nombre);
-    saludo.textContent = 'bienvenido ' + nombre;
-    input.value = ""
-})
-document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuario')
-    if (usuarioGuardado) {
-        saludo.textContent = "Bienvenido nuevamente querido y estimado " + usuarioGuardado;
-
+//  construimos un objeto con caracteristicas o claves y valores
+    const nuevoCurso = {
+        nombre : curso,
+        profesor: profesor,
+        precio: precio,
+        ciudad: ciudad,
+        cupo: cupo
     }
+
+    localStorage.setItem('curso', JSON.stringify (nuevoCurso));
+    form.reset()
 })
-borrar.addEventListener("click", () => {
-    localStorage.removeItem('usuario')
-    saludo.textContent = ('Bienvenido usuario indefinido ')
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const cursoCreado = localStorage.getItem('curso');
+    if (cursoCreado) {
+        const objetoCurso = JSON.parse(cursoCreado);
+        mensaje.innerHTML = 'curso:' + objetoCurso.nombre +'<br>'+' profesor:' + objetoCurso.profesor + '<br>'+' precio:' + objetoCurso.precio +'<br>'+' ciudad:' +objetoCurso.ciudad +'<br>'+' cupo:' +objetoCurso.cupo;
+        
+    }
 })
--- perfil.js
+++
@@ -1,9 +0,0 @@
-const usuario2 = document.getElementById('usuario-creado')
-
-document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuario')
-    if (usuarioGuardado) {
-        usuario2.textContent = "Bienvenido nuevamente querido y estimado " + usuarioGuardado;
-
-    }
-})
